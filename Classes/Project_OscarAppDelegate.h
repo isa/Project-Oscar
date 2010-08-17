823c29fe99c69d8aae990367a260dc2ed208faf2
Isa Goksu <info@isagoksu.com>
2010-08-16 20:01:05 -0600

    Initial Project check in

---
+++ Classes/Project_OscarAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  Project_OscarAppDelegate.h
+//  Project-Oscar
+//
+//  Created by Isa Goksu on 10-08-16.
+//  Copyright Isa Goksu 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Project_OscarAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Project-Oscar
+//
+//  Created by Isa Goksu on 10-08-16.
+//  Copyright Isa Goksu 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
